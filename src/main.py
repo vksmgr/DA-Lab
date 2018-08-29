acf5e5a78bd944b7c152bfd0592a407a80ee48f3
vikas <magar_vikas.ghrcecs@raisoni.net>
2018-08-29 11:36:57 +0530

    upadte

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 
-data_test = pd.DataFrame(np.random.randn(15).reshape((3,5)))
-print(data_test)
+# Hierarchical indexing:
 
-print('everything will work fine.!')
+data_series = pd.Series(np.random.randn(9),
+                        index=[['a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'], [1, 1, 3, 3, 2, 2, 3, 4, 1]])
+print(data_series)
