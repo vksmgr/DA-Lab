2256a44211865958e20756276d29ebfb8ef14422
vikas <magar_vikas.ghrcecs@raisoni.net>
2018-08-29 13:53:01 +0530

    upadte 2

--- README.md
+++ README.md
@@ -1,21 +1,6 @@
 
 ## Hierarchical Indexing :
    - It will allow you to have multiple indexes to work with.
+   - It will allow you to work with higher dimentional data in the lowe dimentional.
    
-a  1   -0.661052
-   1    1.141471
-   3   -0.279373
-b  3   -0.871816
-   2    2.767949
-c  2    0.781526
-   3    0.235020
-d  4    0.992676
-   1    1.044627
-   
-Like this.
-```angular2html
 
-data_series = pd.Series(np.random.randn(9),
-                        index=[['a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'], [1, 1, 3, 3, 2, 2, 3, 4, 1]])
-print(data_series)
-```
--- src/main.py
+++ src/main.py
@@ -4,5 +4,130 @@ import pandas as pd
 # Hierarchical indexing:
 
 data_series = pd.Series(np.random.randn(9),
-                        index=[['a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'], [1, 1, 3, 3, 2, 2, 3, 4, 1]])
+                        index=[['a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd'], [1, 2, 3, 1, 3, 1, 2, 2, 3]])
 print(data_series)
+
+# to see which type of the index it is using you
+print(data_series.index)
+
+# Indexing the multilevel
+
+print(data_series['a', 1])
+
+#converting series to the Dataframe
+print(data_series.drop_duplicates().unstack())
+
+#converting a dataframe to a series
+data_frame = pd.DataFrame(np.random.randn(9).reshape((3,3)))
+print(data_frame)
+print(data_frame.stack())       # simply this method will use the multilevel indexin to convert dataframe to series
+
+#naming hierarchy
+data_series.index.names = ['Level 1', 'Level 2']
+print(data_series)
+
+
+#if you want to swap the level's only then we can use swaplevel()
+# sorting index leel by level sort_index() will take the level and sort the data in that level
+print(data_series.sort_index(level=0)) # data is sorted according o this level
+
+print(data_series.swaplevel(1,0))
+
+#summary statistics
+print(data_series.sum(level='Level 2'))
+
+
+#Indexing through Columns
+print('============================================Column indexing==================')
+
+print(data_frame)
+
+print(data_frame.set_index([0, 1], drop=False))
+print(data_frame.reset_index())
+
+#######
+# Performing Join On Data : mergin Files
+#######
+
+data_frame_1 = pd.DataFrame({'Key':[ 'b', 'b', 'a', 'c', 'a', 'a', 'b'],
+                             'data1': range(7)})
+
+data_frame_2 = pd.DataFrame({'Key': ['a', 'b', 'c'],
+                             'data2': range(3)})
+
+# Performing Merge
+print(data_frame_1)
+print(data_frame_2)
+
+print(pd.merge(data_frame_1,data_frame_2, on='Key', how='inner', indicator=True))
+
+## Join on Hierarchical indexed data
+
+lefth = pd.DataFrame({'key1': ['MH', 'TN', 'GOA', 'UP', 'MP'], 'key2': [2000, 2001, 2002, 2001, 2002],
+                      'data': np.arange(5.)})
+righth = pd.DataFrame(np.arange(12).reshape((6,2)),
+                      index=[['MH', 'MH','GOA', 'GOA', 'GOA','HP'],[2001, 2000, 2000, 2000, 2001, 2000]],
+                      columns=['event1', 'event2'])
+print(lefth)
+
+print(righth)
+
+
+
+print(pd.merge(lefth, righth, left_on=['key1', 'key2'], right_index=True, how='outer'))
+
+left2 = pd.DataFrame([[7,8], [9,2],[3,4]],
+                     index=['a', 'b', 'c'],
+                     columns=['MH', 'DH'])
+right2 = pd.DataFrame([[1, 2], [3, 4], [5, 6]],
+                      index=['a', 'e', 'd'],
+                      columns=['PJ','GU'])
+
+print(left2)
+print(right2)
+
+print(left2.join(right2 , how='outer'))
+
+
+### Concatination Stacking Or Binding
+
+np_array = np.arange(12).reshape((3,4))
+print(np_array)
+
+print(np.concatenate([np_array, np_array], axis=1))
+
+a = pd.Series([np.nan, 2.5, np.nan, 3.4, 4.5, np.nan],
+              index=['f', 'e', 'd', 'c', 'b', 'a'])
+
+b = pd.Series(np.arange(len(a), dtype= np.float64),
+              index=['f', 'e', 'd', 'c', 'b', 'a'])
+
+print(a)
+print(b)
+b[-1] = np.nan
+print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
